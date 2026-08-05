45a3913c30e3cb8e477038f85d459c349fbadf40
emreabacioglu <120939127+emreabacioglu@users.noreply.github.com>
2026-08-05 15:42:37 +0300

    refactor(api): setup Vite environment variable for base URL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://localhost:7095/api',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:7095/api'
 });
 
 export default api;
